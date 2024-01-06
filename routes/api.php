f1e97d01ad5d78c7cb56c50c8e37eed2cc3f1867
xfire5000 <xfire5000@gmail.com>
2024-01-06 22:53:56 +0330

    update pint includes, reformat codes

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum',HandlePrecognitiveRequests::class])->group(function() {
-  // user
-  Route::get('/user', function (Request $request) {
-    return $request->user();
-  });
-  // media-libraries
-  Route::resource('media-library', MediaLibraryController::class)->middleware(['view-medias','add-medias','edit-medias','delete-medias'])
-    ->only(['index','store','update','destroy']);
-  // users
-  Route::resource('users',UserController::class)->middleware(['add-users','edit-users','delete-users'])
-    ->only(['store','update','destroy']);
+Route::middleware(['auth:sanctum', HandlePrecognitiveRequests::class])->group(function () {
+    // user
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    // media-libraries
+    Route::resource('media-library', MediaLibraryController::class)->middleware(['view-medias', 'add-medias', 'edit-medias', 'delete-medias'])
+        ->only(['index', 'store', 'update', 'destroy']);
+    // users
+    Route::resource('users', UserController::class)->middleware(['add-users', 'edit-users', 'delete-users'])
+        ->only(['store', 'update', 'destroy']);
 });
--- routes/panel.php
+++ routes/panel.php
@@ -5,18 +5,18 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware([
-  'auth:sanctum',
-  config('jetstream.auth_session'),
-  'verified',
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
 ])->group(function () {
-  Route::get('/dashboard', function () {
-      return Inertia::render('Dashboard');
-  })->name('dashboard');
-  // users
-  Route::get('users','\App\Http\Controllers\UserController@index')->name('users.index');
-  // media-library download
-  Route::controller(MediaLibraryController::class)->group(function () {
-    Route::get('media-library/download/{id}','generate_url')->name('media-library.download');
-    Route::get('local/temp/{path}', 'download')->name('local.temp');
-  })->middleware('auth:sanctum');
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+    // users
+    Route::get('users', '\App\Http\Controllers\UserController@index')->name('users.index');
+    // media-library download
+    Route::controller(MediaLibraryController::class)->group(function () {
+        Route::get('media-library/download/{id}', 'generate_url')->name('media-library.download');
+        Route::get('local/temp/{path}', 'download')->name('local.temp');
+    })->middleware('auth:sanctum');
 });
